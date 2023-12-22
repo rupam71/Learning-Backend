6a90230d97904023e20f10077ace85cc30402f70
sahrairgrype <rupamsahriar@gmail.com>
2023-12-23 02:12:01 +0600

    add mongodb config file

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,43 @@
-import mongoose, { ConnectOptions } from 'mongoose';
+import Mongoose from "mongoose";
+import MongodbConfiguration from "./mongodbConfiguration";
+Mongoose.Promise = global.Promise;
 
-export default async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI as string, {
-      useNewUrlParser: true,
-      // useCreateIndex: true,
-      useUnifiedTopology: true,
-      // useFindAndModify: false,
-    } as ConnectOptions);
-    console.log('Database Connected');
-  } catch (error) {
-    console.log('Database Error :: ', error);
-  }
+Mongoose.connection.on("disconnected", () => {
+  console.error(
+    `!!!!!!!!!! Database Disconnected @ ${MongodbConfiguration.databaseConnection.uri()} !!!!!!!!!!`
+  );
+});
+Mongoose.connection.on("reconnected", () => {
+  console.warn(
+    `!!!!!!!!!! Database Reconnected @ ${MongodbConfiguration.databaseConnection.uri()} !!!!!!!!!!`
+  );
+});
+Mongoose.connection.on("close", () => {
+  console.log("Connection Closed");
+});
+Mongoose.connection.on("error", (err) => {
+  console.error("error", err);
+});
+
+const db = async () => {
+  await Mongoose.connect(
+    MongodbConfiguration.databaseConnection.uri() as string,
+    {
+      authSource: process.env.MONGO_DB_AUTHDB || "admin",
+      user: process.env.MONGO_DB_USER || "devops",
+      pass: process.env.MONGO_DB_PASSWORD || "devops007",
+    }
+  );
+
+  const db = Mongoose.connection;
+
+  db.on("error", console.error.bind(console, "Connection Error:"));
+
+  db.once("open", () => {
+    console.log(
+      `${process.env.NAME?.toUpperCase()}: Connected to Database @ ${MongodbConfiguration.databaseConnection.uri()}`
+    );
+  });
 };
+
+export default db;

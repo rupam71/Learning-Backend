7682aeb581cea696c7909bee0e489e97be926b81
sahrairgrype <rupamsahriar@gmail.com>
2024-02-13 12:23:56 +0600

    config format

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,10 +3,14 @@ import MongodbConfiguration from "./mongodbConfiguration";
 Mongoose.Promise = global.Promise;
 
 Mongoose.connection.on("disconnected", () => {
-  console.error(`!!!!!!!!!! Database Disconnected @ ${MongodbConfiguration.databaseConnection.uri()} !!!!!!!!!!`);
+  console.error(
+    `!!!!!!!!!! Database Disconnected @ ${MongodbConfiguration.databaseConnection.uri()} !!!!!!!!!!`,
+  );
 });
 Mongoose.connection.on("reconnected", () => {
-  console.warn(`!!!!!!!!!! Database Reconnected @ ${MongodbConfiguration.databaseConnection.uri()} !!!!!!!!!!`);
+  console.warn(
+    `!!!!!!!!!! Database Reconnected @ ${MongodbConfiguration.databaseConnection.uri()} !!!!!!!!!!`,
+  );
 });
 Mongoose.connection.on("close", () => {
   console.log("Connection Closed");
@@ -16,18 +20,27 @@ Mongoose.connection.on("error", (err) => {
 });
 
 const db = async () => {
-  await Mongoose.connect(MongodbConfiguration.databaseConnection.uri() as string, {
-    authSource: process.env.MONGO_DB_AUTHDB || "admin",
-    user: process.env.MONGO_DB_USER || "devops",
-    pass: process.env.MONGO_DB_PASSWORD || "devops007",
-  }).then((ele) => console.log(`0. ${process.env.NAME?.toUpperCase()}: Connected to Database @ ${MongodbConfiguration.databaseConnection.uri()}`));
+  await Mongoose.connect(
+    MongodbConfiguration.databaseConnection.uri() as string,
+    {
+      authSource: process.env.MONGO_DB_AUTHDB || "admin",
+      user: process.env.MONGO_DB_USER || "devops",
+      pass: process.env.MONGO_DB_PASSWORD || "devops007",
+    },
+  ).then((ele) =>
+    console.log(
+      `0. ${process.env.NAME?.toUpperCase()}: Connected to Database @ ${MongodbConfiguration.databaseConnection.uri()}`,
+    ),
+  );
 
   const db = Mongoose.connection;
 
   db.on("error", console.error.bind(console, "Connection Error:"));
 
   db.once("open", () => {
-    console.log(`${process.env.NAME?.toUpperCase()}: Connected to Database @ ${MongodbConfiguration.databaseConnection.uri()}`);
+    console.log(
+      `${process.env.NAME?.toUpperCase()}: Connected to Database @ ${MongodbConfiguration.databaseConnection.uri()}`,
+    );
   });
 };
 

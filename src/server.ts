778c4539d312491bb3bc7c3dbd48585d209e6b36
sahrairgrype <rupamsahriar@gmail.com>
2024-02-24 17:11:07 +0600

    google callback url store in env

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_SECRET_ID || "",
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || "",
     },
     async (accessToken, refreshToken, profile, done) => {
       // Code to handle user authentication and retrieval
